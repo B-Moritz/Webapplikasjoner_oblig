bdca965930e2995d55fe24d36357309c7acd386f
B-Moritz <bermorols@gmail.com>
2022-09-28 11:25:14 +0200

    Adding lib folder

---
+++ Webapplikasjoner_oblig/TradingLib/AlphaVantageInterface.cs
@@ -0,0 +1,12 @@
+﻿namespace Webapplikasjoner_oblig.TradingLib
+{
+    public class AlphaVantageInterface
+    {
+        private static readonly string _apiToken = "";
+
+        private static int _counter = 0;
+
+
+
+    }
+}
