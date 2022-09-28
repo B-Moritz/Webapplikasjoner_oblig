907f8ec979f184fcf1d2a2361920ac0897a38b93
Awet9I <89390508+Awet9I@users.noreply.github.com>
2022-09-28 10:05:25 +0200

    Stock detail poco created

---
+++ Webapplikasjoner_oblig/Model/StockDetails.cs
@@ -0,0 +1,11 @@
+﻿namespace Webapplikasjoner_oblig.Model
+{
+    public class StockDetails
+    {
+        public string StockId { get; set; }
+        public string StockName { get; set; }
+        public string StockSymbol { get; set; }
+        public string Description { get; set; }
+
+    }
+}
