54f99541ef9a17c2e90c089397cb9a678a19f38a
Awet9I <89390508+Awet9I@users.noreply.github.com>
2022-09-28 10:48:28 +0200

    trading context created

--- Webapplikasjoner_oblig/DAL/TradingContext.cs
+++ Webapplikasjoner_oblig/DAL/TradingContext.cs
@@ -1,6 +1,22 @@
-﻿namespace Webapplikasjoner_oblig.DAL
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Webapplikasjoner_oblig.DAL
 {
-    public class TradingContext
+    public class TradingContext : DbContext
     {
+        public class Stock
+        {
+            public string StockId { get; set; }
+            public string StockName { get; set; }
+            public string StockSymbol { get; set; }
+            public string Description { get; set; }
+        }
+
+        
+
+        public TradingContext(DbContextOptions<TradingContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
     }
 }
