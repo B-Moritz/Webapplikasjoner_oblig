378629a5272a18966a32d9e4e1f0afdc3a6817a1
okabmussie <s331690@oslomet.no>
2022-09-29 21:38:54 +0200

    Update IUserRepository.cs
    interfaces

--- Webapplikasjoner_oblig/DAL/IUserRepository.cs
+++ Webapplikasjoner_oblig/DAL/IUserRepository.cs
@@ -1,9 +1,16 @@
 ﻿using System;
+using Webapplikasjoner_oblig.Model;
+
 namespace Webapplikasjoner_oblig.DAL
 {
     public interface IUserRepository
     {
         // test
+        Task<bool> Lagre(User innKunde);
+        Task<List<User>> HentAlle();
+        Task<bool> Delete(int id);
+        Task<User> HentEn(int id);
+        Task<bool> Endre(User changeUser);
     }
 }
 
