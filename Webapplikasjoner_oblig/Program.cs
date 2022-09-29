163d3819fe87449c132f4eff99e3608881efcd80
Awet9I <89390508+Awet9I@users.noreply.github.com>
2022-09-29 14:46:52 +0200

    database kontekst satt opp

--- Webapplikasjoner_oblig/Program.cs
+++ Webapplikasjoner_oblig/Program.cs
@@ -1,8 +1,18 @@
+
+using Microsoft.EntityFrameworkCore;
+using Webapplikasjoner_oblig.DAL;
+
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddControllers();
 
+builder.Services.AddDbContext<TradingContext>(options => options.UseSqlite("Name=WebApiDatabase"));
 
 var app = builder.Build();
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();   
 
 app.MapGet("/", () => "Hello World!");
 
