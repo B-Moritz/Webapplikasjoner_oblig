21a72b20eadcdac0ded4719699c2cfdfda7c072c
okabmussie <s331690@oslomet.no>
2022-10-05 08:56:11 +0200

    frontend
    new frontend

---
+++ Webapplikasjoner_oblig/Model/Trades.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace Webapplikasjoner_oblig.Model
+{
+    public class Trades
+    {
+        public int Id { get; set; }
+        public string StockName { get; set; }
+        public string Date { get; set; }
+        public string User { get; set; }
+
+    }
+}
+
