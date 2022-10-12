ed6fbe66c98e8461e6e90174a2beb4e0aae9f9d7
okabmussie <s331690@oslomet.no>
2022-10-12 18:33:25 +0200

    updates

--- Webapplikasjoner_oblig/Controllers/TradingController.cs
+++ Webapplikasjoner_oblig/Controllers/TradingController.cs
@@ -37,7 +37,7 @@ namespace Webapplikasjoner_oblig.Controllers
 
         // okab ... fra
         [HttpGet]
-        public async Task<Portfolio> GetPortfolioAsync(int userId)
+        public async Task<Portfolio> GetPortfolio(int userId)
         {
             return await _db.GetPortfolioAsync(userId);
         
--- Webapplikasjoner_oblig/DAL/TradingRepository.cs
+++ Webapplikasjoner_oblig/DAL/TradingRepository.cs
@@ -30,6 +30,7 @@ namespace Webapplikasjoner_oblig.DAL
                     // var stock_name = min_stock.UsersId;
                     var stock = min_stock.Stock;
 
+                   
                     var current_Portfolio_stock = new PortfolioStock
                     {
                         StockCounter = min_stock.StockCounter,
@@ -38,7 +39,7 @@ namespace Webapplikasjoner_oblig.DAL
                         Symbol = stock.Symbol,
                         StockName = stock.StockName,
                         Description = stock.Description
-
+                        
                     };
 
                     portfolio_list.Add(current_Portfolio_stock);
@@ -84,6 +85,9 @@ namespace Webapplikasjoner_oblig.DAL
                     Stocks = portfolio_list
                 };
                 return OutPortfolio;
+
+
+
 
             }
             catch
