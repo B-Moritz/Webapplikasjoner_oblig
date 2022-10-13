85d3a7f99e8d503ae18983c1da00f521a073a7a1
okabmussie <s331690@oslomet.no>
2022-10-13 13:09:28 +0200

    porfolio.js

--- Webapplikasjoner_oblig/wwwroot/js/portfolio.js
+++ Webapplikasjoner_oblig/wwwroot/js/portfolio.js
@@ -2,37 +2,34 @@
     printAllMyPortfolio();
 });
 function printAllMyPortfolio() {
-    $.get("Trading/getPortfolio", function (my_stocks) {
-        formatPortfolio(my_stocks);
+    $.get("trading/getPortfolio", function (minStocks){
+        formatPortfolio(minStocks);
     });
 }
 
-function formatPortfolio(my_stócks) {
+function formatPortfolio(minStocks){
     let ut = "<table class='table table-striped'>" +
         "<tr>" +
         "<th>Stock_Name</th><th>Discriptions</th><th>value</th><th>Values</th><th></th>" +
         "</tr>";
 
-    for (let trading of my_stócks) {
+    for (let trading of minStocks) {
         ut += "<tr>" +
             "<td>" + trading.LastUpdate + "</td>" +
             "<td>" + trading.TotalValueSpent + "</td>" +
             "<td>" + trading.TotalPortfolioValue + "</td>" +
             "<td>" + trading.Stocks + "</td>" +
-
-            /*
-                        "<td> <a class='btn btn-primary' href='endre.html?id="+stock.id+"'>sell</a></td>"+
-            */
             "<td> <button class='btn btn-danger' onclick='sellStock(" + trading.id + ")'>sell</button></td>" +
 
             "</tr>";
     }
+
     ut += "</table>";
     $("#portfolios").html(ut);
 }
 
-function slettKunde(id) {
-    const url = "Trading/Slett?id=" + id;
+/*function slettKunde(id) {
+    const url = "trading/SellStock?id=" + id;
     $.get(url, function (OK) {
         if (OK) {
             window.location.href = 'portfolio.html';
@@ -42,5 +39,5 @@ function slettKunde(id) {
         }
 
     });
-};
+};*/
 
