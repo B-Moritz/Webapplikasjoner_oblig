b254b4c3720033006dd93dede2e336fd32c9b578
Awet9I <89390508+Awet9I@users.noreply.github.com>
2022-10-13 20:22:26 +0200

    getonekeyword commented

--- Webapplikasjoner_oblig/DAL/SearchResultRepositry.cs
+++ Webapplikasjoner_oblig/DAL/SearchResultRepositry.cs
@@ -20,28 +20,32 @@ namespace Webapplikasjoner_oblig.DAL
             }
 
             try { 
-            
+            // get a search result that has primary key og keyword
                 SearchResults searchResult = await _db.SearchResults.FindAsync(keyWord);
                 var stockDList = new List<StockDetail>();
 
+                //go over the list of stock the searchreasult holds
                 foreach (var searchRes in searchResult.Stocks)
                 {
 
+                    //make each stock in to stockdetail object with ony neccessary properties extracted
                     var stockDetail = new StockDetail()
                     {
                         StockSymbol = searchRes.Symbol,
                         StockName = searchRes.StockName,
                     };
 
-
+                    //add stockDetail in to a list
                     stockDList.Add(stockDetail);
 
                 };
 
+                //from searchResults to searchResult object
                 var dbSearchResult = new SearchResult()
                 {
                     SearchKeyword = searchResult.SearchKeyword,
                     SearchTime = searchResult.SearchTimestamp,
+                    //list containing searchResult objects
                     StockList = stockDList
 
                 };
